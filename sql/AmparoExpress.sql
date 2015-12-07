7e674e0aa4feb1bbd3dbcb78015d855e466e60f1
Toño Castro <tonoescom@gmail.com>
2015-12-06 20:32:23 -0600

    adding the index and script sql

---
+++ sql/AmparoExpress.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-12-2015 a las 02:24:58
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `AmparoExpress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Admin`
+--
+
+CREATE TABLE IF NOT EXISTS `Admin` (
+  `id_admin` int(11) NOT NULL,
+  `email` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `password` text COLLATE utf8_spanish_ci NOT NULL,
+  `role` varchar(45) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Conekta`
+--
+
+CREATE TABLE IF NOT EXISTS `Conekta` (
+  `id` int(11) NOT NULL,
+  `coneckta_token` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` varchar(45) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Notifications`
+--
+
+CREATE TABLE IF NOT EXISTS `Notifications` (
+  `id` int(11) NOT NULL,
+  `content` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `sent` tinyint(1) NOT NULL,
+  `date_sent` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `User`
+--
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `phone_number` varchar(45) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Admin`
+--
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indices de la tabla `Conekta`
+--
+ALTER TABLE `Conekta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `Notifications`
+--
+ALTER TABLE `Notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Conekta`
+--
+ALTER TABLE `Conekta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `User`
+--
+ALTER TABLE `User`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Conekta`
+--
+ALTER TABLE `Conekta`
+  ADD CONSTRAINT `Conekta_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id_user`);
+
+--
+-- Filtros para la tabla `Notifications`
+--
+ALTER TABLE `Notifications`
+  ADD CONSTRAINT `Notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id_user`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
