d5fb0044bf777cac9029be1e29c5d31334ac51ed
Toño Castro <tonoescom@gmail.com>
2015-12-14 02:01:46 -0600

    adding libraty to conekta

--- model/registro.php
+++ model/registro.php
@@ -1,50 +1,51 @@
 <?php error_reporting(E_ALL);
-include("../model/conexion.php");
-$name = $_POST['Nombre']; 
-$email = $_POST['email'];
-# Include the Autoloader (see "Libraries" for install instructions)
-require '../vendor/autoload.php';
-use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun('key-4e896b8110f1ceb63b180c87bc225c16');
-$domain = "sandbox8128d26fd2464b42a6dc95e54a7cda2a.mailgun.org";
-
-# Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-    'from'    => 'AmparoExpress <postmaster@sandbox8128d26fd2464b42a6dc95e54a7cda2a.mailgun.org>',
-    'to'      => $email,
-    'subject' => 'AmparoExpress',
-    'text'    => 'Hola '.$name.',
-
-
-¡Gracias por confiar en amparoexpress!
-
-Tu amparo ya está en trámite y el proceso de tu liberación puede tardar de 3 a 5 horas*.
-
-Para cualquier duda o aclaración contáctanos a contacto@amparoexpress.com.
-
-Estamos para apoyarte,
-Equipo de amparoexpress
-
-*Este tiempo no depende de amparoexpress; está sujeto a la carga de trabajo del Poder Judicial de la Federación. '
+require_once("../conekta-php/lib/Conekta.php");
+Conekta::setApiKey("key_eYvWV7gSDkNYXsmr");
+$charge = Conekta_Charge::create(array(
+  'description'=> 'Stogies',
+  'reference_id'=> '9839-wolf_pack',
+  'amount'=> 20000,
+  'currency'=>'MXN',
+  'card'=> 'tok_test_visa_4242',
+  'details'=> array(
+    'name'=> 'Arnulfo Quimare',
+    'phone'=> '403-342-0642',
+    'email'=> 'logan@x-men.org',
+    'customer'=> array(
+      'logged_in'=> true,
+      'successful_purchases'=> 14,
+      'created_at'=> 1379784950,
+      'updated_at'=> 1379784950,
+      'offline_payments'=> 4,
+      'score'=> 9
+    ),
+    'line_items'=> array(
+      array(
+        'name'=> 'Box of Cohiba S1s',
+        'description'=> 'Imported From Mex.',
+        'unit_price'=> 20000,
+        'quantity'=> 1,
+        'sku'=> 'cohb_s1',
+        'category'=> 'food'
+      )
+    ),
+    'billing_address'=> array(
+      'street1'=>'77 Mystery Lane',
+      'street2'=> 'Suite 124',
+      'street3'=> null,
+      'city'=> 'Darlington',
+      'state'=>'NJ',
+      'zip'=> '10192',
+      'country'=> 'Mexico',
+      'tax_id'=> 'xmn671212drx',
+      'company_name'=>'X-Men Inc.',
+      'phone'=> '77-777-7777',
+      'email'=> 'purshasing@x-men.org'
+    )
+  )
 ));
 
 
-
+echo $charge->status;
 ?>
 
-//// Change database 
-//mysqli_select_db($con,"$dbname");
-//
-////
-//$query=mysqli_query($con,"INSERT INTO User (email,name) 
-//VALUES ('$email','$name')");
-//
-//mysqli_close($con);
-//
-//header('Location:../index.html?status=success');
-//if( $_GET['status'] == 'success'):
-//    echo 'feedback message goes here';
-//endif;
-?>
