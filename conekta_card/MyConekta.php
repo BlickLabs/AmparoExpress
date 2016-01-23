1ce28c38f5fef201f8b335228cb5406a70ff55e8
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-01-23 16:54:59 -0600

    modifying implementation to payment

--- conekta_card/MyConekta.php
+++ conekta_card/MyConekta.php
@@ -2,108 +2,52 @@
 
 require_once("../conekta/lib/Conekta.php");
 Conekta::setLocale('es');
+Conekta::setApiKey("key_bxBpAz1j8Y3iVW6esPPLxQ");
 
-class MyConekta {
-
-    public static $api_key = 'key_eYvWV7gSDkNYXsmr';
-    public static $description = 'Donation';
-    public static $currency = 'MXN';
-
-    //Function to validate if the token does exist
-    public static function check_token($token) {
-        if ($token == $_SESSION['token'])
-            return true;
-
-        return false;
-    }
-
-    //Function to generate a md5 32digits token
-    public static function tokengenerator($len = 32) {
-        //seed
-        $keychars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        // RANDOM KEY GENERATOR
-        $id = "";
-        $max = strlen($keychars) - 1;
-
-        for ($i = 0; $i < $len; $i++)
-            $id .= substr($keychars, rand(0, $max), 1);
-
-        return md5($id);
-    }
-
-    //Function to make payments in Bank
-    public static function bank($amount, $name, $email, $phone, $bank) {
-        Conekta::setApiKey(self::$api_key);
-        $request = array(
-            'amount' => $amount,
-            'currency' => self::$currency,
-            'description' => self::$description,
-            'details' => array(
-                'name' => $name,
-                'email' => $email,
-                'phone' => $phone
-            ),
-            'bank' => array('type' => $bank)
-        );
-        var_dump($request);
-        try {
-            $response = Conekta_Charge::create($request);
-
-            echo
-            'status=' . $response['status'] .
-            '&currency=' . $response['currency'] .
-            '&description=' . $response['description'] .
-            '&amount=' . $response['amount'] .
-            '&service_name=' . $response['payment_method']['service_name'] .
-            '&service_number=' . $response['payment_method']['service_number'] .
-            '&reference=' . $response['payment_method']['reference'] .
-            '&type=' . $response['payment_method']['type'] .
-            '&expiry_date=' . date('d/m/Y') .
-            '&token=' . $_SESSION['token'];
-        } catch (Exception $e) {
-            // Catch all exceptions including validation errors.
-            echo $e->getMessage();
-        }
-    }
-
-    //Function to make payments with a Credit/Debit Card
-    public static function card($amount, $number, $exp_month, $exp_year, $cvc, $name) {
-
-        Conekta::setApiKey(self::$api_key);
-
-        $data = array(
-            'card' => array(
-                'number' => $number,
-                'exp_month' => $exp_month,
-                'exp_year' => $exp_year,
-                'cvc' => $cvc,
-                'name' => $name
-            ),
-            'description' => self::$description,
-            'amount' => $amount,
-            'currency' => self::$currency
-        );
-
-
-        try {
-            $charge = Conekta_Charge::create($data);
-            $token = $charge->status;
-            if ($token == 'paid') {
-            require_once '../model/init.php';
-            require_once '../model/mail_pagos_exitosos.php';
+try{
+    $charge = Conekta_Charge::create(array(
+  'amount'=> 220000,
+  'currency'=>'MXN',
+  'description'=> 'Servicio',
+  'reference_id'=> '9839-wolf_pack',
+  'card'=> $_POST['conektaTokenId'],
+  'details'=> array(
+    'name'=> $_POST['name'],
+    'email'=> $_POST['email'],
+    'customer'=> array(
+      'corporation_name'=> 'Conekta Inc.',
+      'logged_in'=> true,
+      'successful_purchases'=> 14,
+      'created_at'=> 1379784950,
+      'updated_at'=> 1379784950,
+      'offline_payments'=> 4,
+      'score'=> 9
+    ),
+    'line_items'=> array(
+      array(
+        'name'=> 'Box of Cohiba S1s',
+        'description'=> 'Imported From Mex.',
+        'unit_price'=> 20000,
+        'quantity'=> 1,
+        'sku'=> 'cohb_s1',
+        'type'=> 'food'
+      )
+    )
+   )
+  
+    ));
+  $toke=$charge->status;
+  if ($toke=='paid'){
+    //require_once '../model/init.php';
+     //       require_once '../model/mail_pagos_exitosos.php';
             header('Location: ../aprobado.html');
-            }
-        } catch (Exception $e) {
-            // Catch all exceptions including validation errors.
-//            echo $e->getMessage(); 
-//            $error = $charge->status;
-//            echo $error;
-            header('Location: ../rechazado.html');
-        }
-    }
-
-    //END OF CLASS
+  }
+} catch (Exception $ex) {
+//    echo $ex;
+header('Location: ../rechazado.html');
+   
 }
-
-
-
+        
+        
+        
+?>
